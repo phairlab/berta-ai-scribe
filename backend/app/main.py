1bff8d9ef3db553afdb5ede8e46c54e48ea35eed
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-07-22 15:41:09 -0600

    Fix docker compose

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://localhost:3000"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
