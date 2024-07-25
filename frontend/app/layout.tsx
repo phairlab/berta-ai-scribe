99d85f3a77102c4e333d77d4ef22787352a95edc
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-07-25 02:29:38 -0600

    Disable pull-to-refresh on mobile

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning className="overscroll-none" lang="en">
       <head />
       <body
         className={clsx(
