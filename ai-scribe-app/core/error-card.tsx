352aa5e87c777f1bb256f7beff7214328e8c7492
Jesse Dunn <67511299+dataxuf@users.noreply.github.com>
2024-10-11 16:32:45 -0600

    Add instructions to start whisper service if stopped (#89)
    Co-authored-by: Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>

--- ai-scribe-app/core/error-card.tsx
+++ ai-scribe-app/core/error-card.tsx
@@ -1,8 +1,26 @@
 import { Button } from "@nextui-org/button";
+import { Code } from "@nextui-org/code";
 
 import { ApplicationError } from "@/utility/errors";
 
 import { OutputCard } from "./output-card";
+
+const whisperOfflineMessage = (
+  <div className="flex flex-col">
+    <p>
+      The transcription service is currently offline. To restart the serivice,
+      run the following commands in Snowflake, using the RL_TEAM_JENKINS role:
+    </p>
+    <Code>USE DATABASE DB_TEAM_JENKINS;</Code>
+    <Code>USE SCHEMA JENKINS_PROD;</Code>
+    <Code>ALTER SERVICE whisper_service RESUME;</Code>
+    <p>
+      The service can take a few minutes to restart. To view the current status,
+      execute the following command:
+    </p>
+    <Code>DESCRIBE SERVICE whisper_service;</Code>
+  </div>
+);
 
 type ErrorCardProps = {
   error: ApplicationError;
@@ -17,7 +35,15 @@ export const ErrorCard = ({
   retryAction,
   onDismiss,
 }: ErrorCardProps) => {
-  const title = <span className="text-red-500">{error.name}</span>;
+  const isWhisperOfflineError = error.message.startsWith(
+    "Cannot connect to host whisper-service",
+  );
+
+  const title = (
+    <span className="text-red-500">
+      {isWhisperOfflineError ? "Transcription Service Offline" : error.name}
+    </span>
+  );
   const controls = retryAction && (
     <>
       <Button color="default" size="sm" onClick={retryAction}>
@@ -34,7 +60,9 @@ export const ErrorCard = ({
   return (
     <OutputCard controls={controls} title={title}>
       {error.message && (
-        <div className="text-sm font-mono">{error.message}</div>
+        <div className="text-sm font-mono">
+          {isWhisperOfflineError ? whisperOfflineMessage : error.message}
+        </div>
       )}
     </OutputCard>
   );
