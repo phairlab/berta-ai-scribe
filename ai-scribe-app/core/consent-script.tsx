304ef945aaca095b0147f160fb1a5a36bdfdefd4
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-10-23 11:51:43 -0600

    Update consent script and display on AI scribe

---
+++ ai-scribe-app/core/consent-script.tsx
@@ -0,0 +1,19 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+type ConsentScriptProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
+
+export const ConsentScript = ({ ...props }: ConsentScriptProps) => (
+  <div {...props}>
+    <p className="font-bold mb-2">Consent Script:</p>
+    <p>
+      &quot;I have a new AI tool that listens to our conversation and helps me
+      write my notes. I review everything before the notes are placed into your
+      chart. Your informamtion may be used for quality improvement. Saying no to
+      using the AI tool to record our conversation will have no impact on your
+      care. Are you okay with me turning it on?&quot;
+    </p>
+  </div>
+);
