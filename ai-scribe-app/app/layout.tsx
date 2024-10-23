49bc9146b85b22a3e082675c74177d0cc6f83e4c
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-10-23 12:30:00 -0600

    Add AHS logo to footer

--- ai-scribe-app/app/layout.tsx
+++ ai-scribe-app/app/layout.tsx
@@ -6,6 +6,8 @@ import clsx from "clsx";
 
 import { Metadata, Viewport } from "next";
 
+import { Image } from "@nextui-org/image";
+
 import { fontSans } from "@/config/fonts";
 import { siteConfig } from "@/config/site";
 import { Navbar } from "@/core/navbar";
@@ -52,10 +54,11 @@ export default async function RootLayout({
           <div className="relative flex flex-col h-screen w-full max-w-5xl mx-auto">
             <Navbar />
             <main className="w-full h-full pt-3 sm:pt-6 px-6">{children}</main>
-            <footer className="w-full flex items-center justify-center self-end py-3">
-              <p className="text-sm text-center text-default-400">
+            <footer className="w-full flex flex-row gap-6 items-end justify-center py-3">
+              <p className="text-sm text-center text-default-400 mb-1">
                 &copy; {new Date().getFullYear()} Alberta Health Services
               </p>
+              <Image alt="AHS Logo" src="ahs-color-logo.png" width="100px" />
             </footer>
           </div>
         </Providers>
