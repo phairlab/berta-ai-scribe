905ee42074e4a64cd4ac2909c6638a4c55ac29a1
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-11-17 22:28:43 -0700

    Fix markdown/plaintext newline placement

--- ai-scribe-app/utility/converters.ts
+++ ai-scribe-app/utility/converters.ts
@@ -51,6 +51,8 @@ export function fromMarkdownToPlainText(markdown: string) {
     .replace(/\<(.+)( .+)?\>(.*)\<\/\1\>/g, "$3") // Remove HTML tag pairs
     .replace(/\<(.+ )\/\>/g, "") // Remove HTML singleton tags
     .replace(/\\([\\`*_{}\[\]<>()#+-.!|])/g, "$1") // Unescape special characters
-    .replace(/\n+\n\n/g, "\n\n") // Condense multi blank lines
+    .replace(/\n+\n\n/g, "\n\n") // Condense multi-row blank lines
+    .replace(/\n\n(- .*)/g, "\n$1") // Remove blank lines before bullet lists
+    .replace(/\n\n(1\. .*)/g, "\n$1") // Remove blank lines before numeric lists
     .trim();
 }
