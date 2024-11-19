85adcfed68a9190f7620ddc67284d30f360240ed
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-11-18 22:33:19 -0700

    Fix reference conflict

--- web-api/app/routers/tasks.py
+++ web-api/app/routers/tasks.py
@@ -5,17 +5,16 @@ from fastapi import APIRouter, Depends, Body, status, BackgroundTasks
 
 import app.services.error_handling as errors
 import app.schemas as sch
-from app.tasks.transcription import transcribe_audio
-from app.tasks.generation import generate_note
+import app.tasks as tasks
 from app.services.db import new_sqid, useDatabase
-from app.services.security import authenticate_user, useUserSession
+from app.services.security import authenticate_session, useUserSession
 from app.services.logging import WebAPILogger, log_transcription, log_generation
 from app.services.measurement import ExecutionTimer
 from app.config import settings
 
 log = WebAPILogger(__name__)
 
-router = APIRouter(dependencies=[Depends(authenticate_user)], responses={
+router = APIRouter(dependencies=[Depends(authenticate_session)], responses={
     status.HTTP_500_INTERNAL_SERVER_ERROR: {"description": "Internal Server Error", "model": sch.WebAPIError},
     status.HTTP_502_BAD_GATEWAY: {"description": "External Service Error", "model": sch.WebAPIError},
     status.HTTP_503_SERVICE_UNAVAILABLE: {"description": "External Service Unavailable", "model": sch.WebAPIError},
@@ -36,7 +35,7 @@ async def transcribe_audio(
         filepath = Path(settings.RECORDINGS_FOLDER, userSession.username, filename)
 
         with ExecutionTimer() as timer, open(filepath, mode="rb") as file:
-            transcription_output = await transcribe_audio(file, filename, media_type)
+            transcription_output = await tasks.transcribe_audio(file, filename, media_type)
 
         backgroundTasks.add_task(
             log_transcription,
@@ -91,7 +90,7 @@ def generate_draft_note(
     # Get the stream of note segments.
     try:
         noteId = new_sqid(database)
-        generation_output = generate_note(settings.GENERATIVE_AI_MODEL, instructions, transcript, outputType)
+        generation_output = tasks.generate_note(settings.GENERATIVE_AI_MODEL, instructions, transcript, outputType)
         backgroundTasks.add_task(
             log_generation,
             database,
