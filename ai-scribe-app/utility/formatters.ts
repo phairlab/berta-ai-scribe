d8d9a1f4f5b781e35761fc1d6ac4ab0dee344dff
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-11-23 08:53:32 -0700

    Fix date display format

--- ai-scribe-app/utility/formatters.ts
+++ ai-scribe-app/utility/formatters.ts
@@ -20,7 +20,7 @@ export function formatDisplayName(username: string) {
 
 export function formatDatestring(date: Date) {
   date = new Date(date);
-  const formattedDate = `${date.getFullYear()}-${("0" + date.getDate()).slice(-2)}-${("0" + (date.getMonth() + 1)).slice(-2)}`;
+  const formattedDate = `${date.getFullYear()}-${("0" + date.getMonth()).slice(-2)}-${("0" + (date.getDate() + 1)).slice(-2)}`;
   const formattedTime = `${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}`;
 
   return `${formattedDate} ${formattedTime}`;
