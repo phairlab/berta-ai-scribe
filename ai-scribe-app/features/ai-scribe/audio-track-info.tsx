81ace2e844cbf46cca1a0e5c10b6e655ed56a7b3
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-11-24 19:37:03 -0700

    Adjust font weight on audio track info

--- ai-scribe-app/features/ai-scribe/audio-track-info.tsx
+++ ai-scribe-app/features/ai-scribe/audio-track-info.tsx
@@ -23,17 +23,17 @@ export const AudioTrackInfo = ({
 
   return (
     <div className="flex flex-col-reverse sm:flex-row justify-between gap-1 sm:gap-5 md:gap-12 text-xs text-zinc-400 dark:text-zinc-500">
-      <div className="hidden sm:block">{audioId}</div>
+      <div className="font-semibold hidden sm:block">{audioId}</div>
       {title !== null && (
         <div
-          className="grow line-clamp-1 text-ellipse font-semibold text-start sm:text-center"
+          className="grow line-clamp-1 text-ellipse text-start sm:text-center"
           title={title}
         >
           {title}
         </div>
       )}
       <div className="flex flex-row w-full sm:w-fit gap-5 justify-between">
-        <div className="block sm:hidden">{audioId}</div>
+        <div className="font-semibold block sm:hidden">{audioId}</div>
         <div className="flex flex-row gap-1">
           <strong>Duration:</strong>
           {duration ? formatDuration(duration) : "--:--"}
