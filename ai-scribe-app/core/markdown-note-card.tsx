44afbbd3b03f7b509adbb58adb6c29aa9a6fe0c1
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-11-27 10:01:26 -0700

    Separate markdown display formatting into separate component

--- ai-scribe-app/core/markdown-note-card.tsx
+++ ai-scribe-app/core/markdown-note-card.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useMemo, useRef, useState } from "react";
 
-import Markdown from "react-markdown";
-
 import { Button } from "@nextui-org/button";
-import { Divider } from "@nextui-org/divider";
 import { SelectItem } from "@nextui-org/select";
 
 import * as convert from "@/utility/converters";
 
+import { Markdown } from "./markdown";
 import { MobileCompatibleSelect } from "./mobile-compatible-select";
 import { OutputCard } from "./output-card";
 import { DraftNote } from "./types";
@@ -135,83 +133,7 @@ export const MarkdownNoteCard = ({
         <div className="font-mono text-sm">{markdown}</div>
       ) : (
         <div ref={markdownNode}>
-          <Markdown
-            className="flex flex-col gap-1 leading-normal p-0"
-            components={{
-              h1({ node, ...rest }) {
-                return (
-                  // eslint-disable-next-line jsx-a11y/heading-has-content
-                  <h1
-                    className="font-semibold mt-4 first:mt-0 [&+*]:mt-0"
-                    {...rest}
-                  />
-                );
-              },
-              h2({ node, ...rest }) {
-                return (
-                  // eslint-disable-next-line jsx-a11y/heading-has-content
-                  <h2 className="italic mt-4 first:mt-0 [&+*]:mt-0" {...rest} />
-                );
-              },
-              p({ node, ...rest }) {
-                return (
-                  <p
-                    className="mt-4 first:mt-0 [&+ul]:mt-0 [&+ol]:mt-0"
-                    {...rest}
-                  />
-                );
-              },
-              blockquote({ node, ...rest }) {
-                return (
-                  <blockquote
-                    className="[&>p]:my-0 py-1 ms-4 ps-3 border-s-1 flex flex-col gap-4"
-                    {...rest}
-                  />
-                );
-              },
-              pre({ node, ...rest }) {
-                return <pre className="text-sm" {...rest} />;
-              },
-              ul({ node, ...rest }) {
-                return (
-                  <ul
-                    className="list-['-_'] list-outside flex flex-col ps-3 mt-4 first:mt-0"
-                    {...rest}
-                  />
-                );
-              },
-              ol({ node, ...rest }) {
-                return (
-                  <ul
-                    className="list-decimal list-outside flex flex-col ps-6 mt-4 first:mt-0"
-                    {...rest}
-                  />
-                );
-              },
-              hr() {
-                return <Divider className="mx-auto w-[98%]" />;
-              },
-              a({ node, href, title, children, ...rest }) {
-                return (
-                  <span {...rest}>
-                    {children && children !== href
-                      ? `[${children}](${href}${title ? ` "${title}"` : ""})`
-                      : `<${href}>`}
-                  </span>
-                );
-              },
-              img({ node, src, title, alt, ...rest }) {
-                return (
-                  <span {...rest}>
-                    {`![${alt}](${src}${title ? ` "${title}"` : ""})`}
-                  </span>
-                );
-              },
-            }}
-            skipHtml={true}
-          >
-            {markdown}
-          </Markdown>
+          <Markdown>{markdown}</Markdown>
         </div>
       )}
     </OutputCard>
