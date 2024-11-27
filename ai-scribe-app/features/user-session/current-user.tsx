0ea142396377af09f71003e3b43d04c9782b6412
Jesse Dunn <67511299+JesseWDunn@users.noreply.github.com>
2024-11-27 10:11:17 -0700

    Refactor CurrentUser component

--- ai-scribe-app/features/user-session/current-user.tsx
+++ ai-scribe-app/features/user-session/current-user.tsx
@@ -11,27 +11,24 @@ import { SessionDropdown } from "./session-dropdown";
 export const CurrentUser = () => {
   const userSession = useSession();
 
-  return (
-    <>
-      {userSession.state === "Authenticated" && (
-        <SessionDropdown>
-          <p
-            className={clsx(
-              "mt-px text-xs text-zinc-400 cursor-pointer text-ellipsis",
-              "hover:text-zinc-300 dark:hover:text-zinc-500",
-              "max-w-[20vw] md:max-w-[350px]",
-              "overflow-clip md:overflow-hidden",
-            )}
-          >
-            {formatDisplayName(userSession.details.username)}
-          </p>
-        </SessionDropdown>
-      )}
-      {userSession.state === "Authenticating" && (
-        <WaitMessageSpinner size="xs">
-          <span className="hidden sm:visible">Connecting</span>
-        </WaitMessageSpinner>
-      )}
-    </>
+  return userSession.state === "Authenticating" ? (
+    <WaitMessageSpinner size="xs">
+      <span className="hidden sm:visible">Connecting</span>
+    </WaitMessageSpinner>
+  ) : userSession.state === "Authenticated" ? (
+    <SessionDropdown>
+      <p
+        className={clsx(
+          "mt-px text-xs text-zinc-400 cursor-pointer text-ellipsis",
+          "hover:text-zinc-300 dark:hover:text-zinc-500",
+          "max-w-[20vw] md:max-w-[350px]",
+          "overflow-clip md:overflow-hidden",
+        )}
+      >
+        {formatDisplayName(userSession.details.username)}
+      </p>
+    </SessionDropdown>
+  ) : (
+    <></>
   );
 };
